65fcce8375220ef008235f99f0d9c66dbb628810
Lissantanad <153249528+Lissantanad@users.noreply.github.com>
2024-02-18 23:13:09 -0400

    Agregar archivos de proyecto.

---
+++ numeros enteros/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingrese la cantidad de números enteros: ");
+        int cantidad = int.Parse(Console.ReadLine());
+
+        int[] numeros = new int[cantidad];
+
+        for (int i = 0; i < cantidad; i++)
+        {
+            Console.Write($"Ingrese el número {i + 1}: ");
+            numeros[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("Los números ingresados son:");
+        foreach (int numero in numeros)
+        {
+            Console.WriteLine(numero);
+        }
+    }
+}
